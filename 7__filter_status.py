2970d285591426667364e081f070833e35262284
David Green <40466236+davidjohngreen@users.noreply.github.com>
2022-07-22 22:32:19 +0100

    Update 7__filter_status.py

--- 7__filter_status.py
+++ 7__filter_status.py
@@ -1,3 +1,7 @@
+# need to filter the haplotype files to only include specific individuals
+# for haplotype analysis, we only want those with unambiguous individuals
+# for the analysis of common and rare individuals, we only want those with complete genotypes at all three sites
+
 file = open('status-by-ID.txt', 'r')
 out1 = open('status-by-ID-full-genotypes.txt', 'w+')
 out2 = open('status-by-ID-full-unabiguous_haplotypes.txt', 'w+')
