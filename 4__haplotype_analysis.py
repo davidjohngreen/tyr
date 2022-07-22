279993a2e30e66d2b07961f5f85ffd5908ac3a1b
David Green <40466236+davidjohngreen@users.noreply.github.com>
2022-07-22 22:26:31 +0100

    Update 4__haplotype_analysis.py

--- 4__haplotype_analysis.py
+++ 4__haplotype_analysis.py
@@ -1,3 +1,6 @@
+# with genotype data in column format (one column per variant, one row per individual), runs through and assigns homozygous haplotype
+# for example, an individual with genotypes 0/0, 1/1, and 1/1 for the promotor variant (C>T) and two missense changes (G>A and C>A) has a haplotype CAA
+
 file = open('status-by-ID.txt', 'r')
 output = open('haplotypes.txt', 'w+')
 probandFile = open('proband-IDs.txt', 'r')
@@ -6,7 +9,7 @@ probands = []
 for i in probandFile:
     probands.append(i.strip('\n'))
 
-
+# refers to indices of the columns
 var_402 = 0
 var_192 = 1
 modifier = 2
@@ -24,6 +27,7 @@ def genotype_generator(n):
         genotype = [int(i) for i in n.strip().split('/')]
         return sum(genotype)
 
+# skip header, then look at columns 2, 3, and 4 (with indices 0, 1, and 2 as above after making "items" from the line without the first column (LpID)
 for line in file:
     if line.startswith('LPid'):
         continue
@@ -73,9 +77,7 @@ for line in file:
         else:
             IDs[''].append(line.strip().split('\t')[0])
 
-
-
-
+# for each sample ID, print out the ID plus the homozygous haplotype            
 for i in IDs:
     for j in IDs[i]:
         output.write(i + '\t' + j + '\n')
